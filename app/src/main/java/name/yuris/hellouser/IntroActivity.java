7a88e85073f8cfc802c31a35b5fcf1ccb1eb5ab3
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-06-10 22:25:38 +0300

    implemented the first activity to go to the second when phone number filled.

--- app/src/main/java/name/yuris/hellouser/IntroActivity.java
+++ app/src/main/java/name/yuris/hellouser/IntroActivity.java
@@ -2,12 +2,63 @@ package name.yuris.hellouser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.redmadrobot.inputmask.MaskedTextChangedListener;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    private TextView mHintTextView;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        mEditText = (EditText) findViewById(R.id.edit_text);
+        mHintTextView = (TextView) findViewById(R.id.hint_text_view);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mHintTextView.setText(getString(R.string.hint));
+        mHintTextView.setVisibility(View.INVISIBLE);
+
+
+        final MaskedTextChangedListener listener = new MaskedTextChangedListener(
+                "+7 ([000]) [000] [00] [00]",
+                true,
+                mEditText,
+                null,
+                new ValueListener()
+        );
+
+        mEditText.addTextChangedListener(listener);
+        mEditText.setOnFocusChangeListener(listener);
+        mEditText.setHint(listener.placeholder());
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 6/10/2017 Hide keyboard here!
+                startActivity(MainActivity.createExplicitIntent(getApplicationContext()));
+            }
+        });
+        mLoginButton.setEnabled(false);
+    }
+
+    private class ValueListener implements MaskedTextChangedListener.ValueListener{
+
+        @Override
+        public void onTextChanged(boolean maskFilled, String value) {
+            if(maskFilled){
+                mLoginButton.setEnabled(true);
+                mHintTextView.setVisibility(View.INVISIBLE);
+            } else {
+                mLoginButton.setEnabled(false);
+                mHintTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- app/src/main/java/name/yuris/hellouser/MainActivity.java
+++ app/src/main/java/name/yuris/hellouser/MainActivity.java
@@ -1,9 +1,14 @@
 package name.yuris.hellouser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    public static Intent createExplicitIntent (Context context){
+        return new Intent(context, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
