7f69cea9cc7436d6dfbe90655c78de4f50bb94eb
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-06-17 23:39:41 +0300

    added downloading image with Picasso with its placeholder animation and error icon of 5 dimensions

--- app/src/main/java/name/yuris/hellouser/MainActivity.java
+++ app/src/main/java/name/yuris/hellouser/MainActivity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity {
+
+    private static final String IMAGE_URL = "http://i.imgur.com/DvpvklR.png";
+    private ImageView mImageView;
+
     public static Intent createExplicitIntent (Context context){
         return new Intent(context, MainActivity.class);
     }
@@ -14,5 +21,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.android_image_view);
+        Picasso.with(this)
+                .load(IMAGE_URL)
+                .resize(500,500)
+                .centerCrop()
+                .placeholder(R.drawable.progress_animation)
+                .error(R.drawable.error)
+                .into(mImageView);
     }
 }
+
